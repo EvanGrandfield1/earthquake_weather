58874da4f38561ee65de16bea76b89fefe9c43a4
EvanGrandfield <evan.grandfield@quantco.com>
2023-04-20 18:58:08 -0400

    drop dupes because getting part of past hour of data from while loop

--- earthquakes.py
+++ earthquakes.py
@@ -11,7 +11,8 @@ def earthquake(n=3.0):
     method="GET"
     url_base="https://earthquake.usgs.gov/fdsnws/event/1/query?"
     hour_ago = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).isoformat()
-    right_now = datetime.datetime.utcnow() 
+    right_now = (datetime.datetime.utcnow() - datetime.timedelta(minutes=15)).isoformat()
+
     pars = {
            "format": "geojson",
            "starttime": f"{hour_ago}",
@@ -49,13 +50,13 @@ def earthquake(n=3.0):
     #print(df.index.max())
     
     max_index = df.index.max()
-    true_time.sleep(15) # want "right now" to be distinct from above
+    true_time.sleep(10) # want "right now" to be distinct from above
     
     while True:
        
         # go thru every 15 seconds and scan from 15 seconds ago if there are any quakes
         
-        right_now =  (datetime.datetime.utcnow() - datetime.timedelta(seconds=15)).isoformat()
+        right_now =  (datetime.datetime.utcnow() - datetime.timedelta(minutes=15)).isoformat()
 
         pars = {
                "format": "geojson",
@@ -83,17 +84,23 @@ def earthquake(n=3.0):
             # new records, so as we appedn to the list it's easier to recreate 
             # the entire dataframe from begininning or at least past n values
             df = pd.DataFrame(list_of_earthquakes)
+            df = df.drop_duplicates(subset=['title', 'time']) # need to drop dupes if always going 
+            # 15 minutes back while doing it every 15 seconds
+            # this way we can test that this loop works while not printing stuff 2x
             # need to sort so oldest are lowest index because that's what the example shows
             df = df.sort_values(by='time', ascending=True)
             df.reset_index(drop=True, inplace=True)
             df['temp_f'] = df['temp_f'].fillna(70.0) # done per Corey's advice
-            df['cum_sum_of_last_n'] = df.temp_f.rolling(n, min_periods=1).sum()
+            df['cum_sum_of_last_n'] = df.temp_f.rolling(int(n), min_periods=1).sum()
             df['standard_divisor'] = n
             df['index_plus_one'] = df.index + 1.0
             df['used_divisor'] = df[['standard_divisor', 'index_plus_one']].min(axis=1)
             df['sliding_average_of_last_n'] = df['cum_sum_of_last_n'] / df['used_divisor']
             df_new = df[df.index > max_index]
-            print(df_new[['title','time','temp_f','sliding_average_of_last_n']])
+            if df_new.empty:
+                pass
+            else:
+                print(df_new[['title','time','temp_f','sliding_average_of_last_n']])
             max_index = df.index.max()
             
         print('waiting\n')
